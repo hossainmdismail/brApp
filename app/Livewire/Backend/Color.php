a658005a410ae219b7675afec82e6a4a1f92bc8f
esmail <hossainmdismail9010@gmail.com>
2024-03-27 15:34:55 +0600

    color/size crud

--- app/Livewire/Backend/Color.php
+++ app/Livewire/Backend/Color.php
@@ -2,25 +2,90 @@
 
 namespace App\Livewire\Backend;
 
-use Livewire\Attributes\Validate;
+use App\Models\Color as ModelsColor;
 use Livewire\Component;
 
 class Color extends Component
 {
-    #[Validate('required')]
-    public $name = null;
-    #[Validate('required')]
-    public $code = null;
+    public $id      = null;
+    public $name    = null;
+    public $code    = null;
 
 
     public function save()
     {
-        $this->validate();
-        $this->dispatch('stayModal');
+        if ($this->name == null && $this->code == null) {
+            # code...
+            session()->flash('err', 'Form validation faild');
+            $this->dispatch('stay-model');
+            return;
+        }
+
+        $color = new ModelsColor();
+        $color->name = $this->name;
+        $color->code = $this->code;
+        $color->save();
+        $this->reset();
+
+        session()->flash('succ', 'Color added successfully');
+        $this->dispatch('stay-model');
+
+
+        // return;
+        // $this->reset();
+    }
+
+    public function edit($id)
+    {
+        $color = ModelsColor::find($id);
+        if ($color) {
+            $this->name = $color->name;
+            $this->code = $color->code;
+            $this->id = $id;
+            $this->dispatch('edit');
+        }
+    }
+
+    public function update()
+    {
+        $color = ModelsColor::find($this->id);
+        if ($color) {
+            $color->name = $this->name;
+            $color->code = $this->code;
+            $color->save();
+            $this->reset();
+            session()->flash('succ', 'Updated');
+        }
+        $this->dispatch('stay-model');
+    }
+
+    public function delete($id)
+    {
+        $color = ModelsColor::find($id);
+        if ($color) {
+            $this->id = $id;
+            $this->dispatch('delete');
+            return;
+        }
+        session()->flash('err', 'Cant find id');
+        $this->dispatch('stay-model');
+    }
+
+    public function destroy()
+    {
+        $color = ModelsColor::find($this->id);
+        if ($color) {
+            $color->delete();
+            session()->flash('succ', 'Delete successfully');
+        } else {
+            session()->flash('err', 'Cant find id');
+        }
+        $this->dispatch('stay-model');
     }
 
     public function render()
     {
-        return view('livewire.backend.color');
+        $color = ModelsColor::get();
+        return view('livewire.backend.color', ['colors' => $color]);
     }
 }
--- resources/views/backend/attributes/index.blade.php
+++ resources/views/backend/attributes/index.blade.php
@@ -1,6 +1,7 @@
 @extends('backend.master')
 @section('content')
     <section class="content-main row">
+
         {{-- Color --}}
         <livewire:backend.color />
         {{-- Size --}}
